ba7b706858ef6a22ccc83b8b9c27a29f05450c67
roninhaf <roninhaf@gmail.com>
2019-03-06 10:48:59 -0500

    Refactored to avoid nested functions

--- script.js
+++ script.js
@@ -47,85 +47,95 @@ app.appendChild(logo);
 app.appendChild(buttonContainer);
 app.appendChild(container);
 
+//Will be used for the different sorting algos
+var sortVar;
+
+//will be used to store the JSON object
+var data = {};
+
 //Go get the infos
 req=new XMLHttpRequest();
 req.open("GET",'https://swapi.co/api/people/',true);
 req.send();
-req.onload=function(){
+req.onload = function retreiveJson(){
     
-    if (req.status >= 200 && req.status < 400) {
-        
-        //turn infos into an object
-        const data=JSON.parse(req.responseText);
-        
-        //default sort algo
-        var sortVar = data.results.sort((a,b)=>a.name>b.name);
+        if (req.status >= 200 && req.status < 400) {
+            
+            //turn infos into an object
+            data = JSON.parse(req.responseText);
 
-        sortVar.forEach(a => {
+            displayDefaultCards();
             
-            const card = document.createElement('div');
-            card.setAttribute('class', 'card');
-            
-            const h1 = document.createElement('h1');
-            h1.textContent = a.name;
-            
-            const p = document.createElement('p');
-            var html = `<ul><li>${a.height}cm</li><li>${a.mass}kgs</li><li>Appears in ${a.films.length} movies</li></ul>`;
-            p.innerHTML = html;
-            
-            //Adding created elements to container so they will show on screen
-            container.appendChild(card);
-            card.appendChild(h1);
-            card.appendChild(p);
-            });
-        
-            
-            buttonContainer.addEventListener('click', triggerSort);
-            
-            //select new sort algo depending on button clicked on
-            function triggerSort(id) {
-                
-                //console.log(typeof id);
-                switch (id.target.id) {
-                    case 'height':
-                    sortVar = data.results.sort((a,b)=>Number(a.height)>Number(b.height));
-                    //console.log(sortVar);
-                    break;
-                    case 'mass':
-                    sortVar = data.results.sort((a,b)=>Number(a.mass)>Number(b.mass));
-                    //console.log(sortVar);
-                    break;
-                    case 'film':
-                    sortVar = data.results.sort((a,b)=>a.films.length>b.films.length);
-                    //console.log(sortVar);
-                    break;
-                    case 'name':
-                    sortVar = data.results.sort((a,b)=>a.name>b.name);
-                    //console.log(sortVar);
-                    break;
-                    default:
-                    sortVar = data.results.sort((a,b)=>a.name>b.name);
-                }
-                //console.log(sortVar);
-                displayRes();
-            }
-            
-            //Use new sort algo and then replace infos
-            function displayRes() {
-                
-                var i=0;
-                sortVar.forEach(a => {
-                    //console.log(container.childNodes)
-                    container.childNodes[i].childNodes[0].textContent = a.name;
-                    html = `<ul><li>${a.height}cm</li><li>${a.mass}kgs</li><li>Appears in ${a.films.length} movies</li></ul>`;
-                    container.childNodes[i].childNodes[1].innerHTML = html;
-                    i++;
-                })
-            }
-        
-    } else {
-        const errorMessage = document.createElement('h1');
-        errorMessage.textContent = `Nope ! The force is not with you today :-/`;
-        app.appendChild(errorMessage);
+        } else {
+            const errorMessage = document.createElement('h1');
+            errorMessage.textContent = `Nope ! The force is not with you today :-/`;
+            app.appendChild(errorMessage);
         }
-};
+    };
+
+function displayDefaultCards() {
+    
+    //default sort algo
+    sortVar = data.results.sort((a,b)=>a.name>b.name);
+    
+    sortVar.forEach(a => {
+        
+        const card = document.createElement('div');
+        card.setAttribute('class', 'card');
+        
+        const h1 = document.createElement('h1');
+        h1.textContent = a.name;
+        
+        const p = document.createElement('p');
+        var html = `<ul><li>${a.height}cm</li><li>${a.mass}kgs</li><li>Appears in ${a.films.length} movies</li></ul>`;
+        p.innerHTML = html;
+        
+        //Adding created elements to container so they will show on screen
+        container.appendChild(card);
+        card.appendChild(h1);
+        card.appendChild(p);
+    });
+}
+    
+    //Use new sort algo and then replace infos
+function displayTriggerResult() {
+    
+    var i=0;
+    sortVar.forEach(a => {
+        //console.log(container.childNodes)
+        container.childNodes[i].childNodes[0].textContent = a.name;
+        html = `<ul><li>${a.height}cm</li><li>${a.mass}kgs</li><li>Appears in ${a.films.length} movies</li></ul>`;
+        container.childNodes[i].childNodes[1].innerHTML = html;
+        i++;
+    })
+}
+    
+    //select new sort algo depending on button clicked on
+function triggerSort(id) {
+    
+    //console.log(typeof id);
+    switch (id.target.id) {
+        case 'height':
+        sortVar = data.results.sort((a,b)=>Number(a.height)>Number(b.height));
+        //console.log(sortVar);
+        break;
+        case 'mass':
+        sortVar = data.results.sort((a,b)=>Number(a.mass)>Number(b.mass));
+        //console.log(sortVar);
+        break;
+        case 'film':
+        sortVar = data.results.sort((a,b)=>a.films.length>b.films.length);
+        //console.log(sortVar);
+        break;
+        case 'name':
+        sortVar = data.results.sort((a,b)=>a.name>b.name);
+        //console.log(sortVar);
+        break;
+        default:
+        sortVar = data.results.sort((a,b)=>a.name>b.name);
+    }
+    //console.log(sortVar);
+    displayTriggerResult();
+}
+
+buttonContainer.addEventListener('click', triggerSort);
